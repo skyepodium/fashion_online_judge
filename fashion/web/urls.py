3ae34fdd15d04e06c761b68213c6ecd78b8228d3
김정윤 <skyepodium@gmail.com>
2017-08-31 17:37:45 +0900

    https

--- fashion/web/urls.py
+++ fashion/web/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls import include, url
 
 from web.main_page.views import main_page
 from web.evaluation.views import evaluation
+from django.http import HttpResponse
 
 app_name = 'web'
+
+def heart_beat(request):
+    return HttpResponse("It's Works!", content_type='text/plain')
 
 urlpatterns = [
 
 	url(r'^$', main_page, name='main_page'),
 	url(r'^evaluation/$', evaluation, name='evaluation'),
+	url('^heart_beat/', heart_beat),
 
 ]
